cd5160114b1cee399c9fd98d1e00f3fb5c94f666
mike-neck <shinya.mochida@l-is-b.com>
2019-04-04 16:49:31 +0900

    テストデータが異なるケースをテスト

--- junit4/src/test/java/com/example/CardsTest.java
+++ junit4/src/test/java/com/example/CardsTest.java
@@ -49,6 +49,16 @@ public class CardsTest {
   }
 
   @Test
+  public void reFullShuffleOf20Cards() {
+    final var cards = Cards.twoTimes(10);
+
+    final var actual = cards.reFullShuffle();
+
+    assertThat(actual).isEqualTo(Cards.of(
+        1,11,2,12,3,13,4,14,5,15,6,16,7,17,8,18,9,19,10,20));
+  }
+
+  @Test
   public void equality_異なる() {
     final Cards cards = Cards.of(1, 2);
     final Cards another = Cards.of(2, 1);
