cf3d51f3c92d00c3516db4908c657e008089c4d4
mike-neck <shinya.mochida@l-is-b.com>
2019-04-04 11:44:06 +0900

    cutAt(2) のテストを作成。当然落ちた

--- junit4/src/test/java/com/example/CardsTest.java
+++ junit4/src/test/java/com/example/CardsTest.java
@@ -13,6 +13,16 @@ public class CardsTest {
     assertThat(actual).isEqualTo(Cards.of(2,3,4,5,6,1));
   }
 
+
+  @Test
+  public void cutAt2nd() {
+    final Cards cards = Cards.of(1, 2, 3, 4, 5, 6);
+
+    final Cards actual = cards.cutAt(2);
+
+    assertThat(actual).isEqualTo(Cards.of(3,4,5,6,1,2));
+  }
+
   @Test
   public void equality_異なる() {
     final Cards cards = Cards.of(1, 2);
