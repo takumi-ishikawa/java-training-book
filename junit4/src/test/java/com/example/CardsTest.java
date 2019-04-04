1be0e392470abdc5839f9f8dbc9a788a71471d0c
mike-neck <shinya.mochida@l-is-b.com>
2019-04-04 11:00:32 +0900

    等価に関するテストは失敗する

--- junit4/src/test/java/com/example/CardsTest.java
+++ junit4/src/test/java/com/example/CardsTest.java
@@ -12,4 +12,12 @@ public class CardsTest {
     final Cards actual = cards.cutAt(1);
     assertThat(actual).isEqualTo(Cards.of(2,3,4,5,6,1));
   }
+
+  @Test
+  public void equality() {
+    final Cards cards = Cards.of(1, 2);
+    final Cards another = Cards.of(2, 1);
+
+    assertThat(cards).isNotEqualTo(another);
+  }
 }
