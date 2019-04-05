87bbe412268974692e6eece8940d772544d42ced
mike-neck <shinya.mochida@l-is-b.com>
2019-04-05 15:15:51 +0900

    テスト用の Cards インターフェースを作る

--- junit4/src/test/java/com/example/OperationTest.java
+++ junit4/src/test/java/com/example/OperationTest.java
@@ -9,10 +9,42 @@ public class OperationTest {
   @Test
   public void reFullShuffleはreFullShuffleを呼び出す() {
     final Operation operation = new ReFullShuffle();
-    final Cards cards = CardsImpl.of(1,2,3,4);
+    final Cards cards = new Shuffle();
 
     final var actual = operation.doOp(cards);
 
-    assertThat(actual).isEqualTo(CardsImpl.of(1,3,2,4));
+    assertThat(actual).isInstanceOf(Shuffle.class);
+  }
+
+  static class Shuffle implements Cards {
+
+    @Override
+    public Cards cutAt(final int position) {
+      return new Cut(position);
+    }
+
+    @Override
+    public Cards reFullShuffle() {
+      return this;
+    }
+  }
+
+  static class Cut implements Cards {
+
+    final int pos;
+
+    Cut(final int pos) {
+      this.pos = pos;
+    }
+
+    @Override
+    public Cards cutAt(final int position) {
+      return new Cut(position);
+    }
+
+    @Override
+    public Cards reFullShuffle() {
+      return new Shuffle();
+    }
   }
 }
