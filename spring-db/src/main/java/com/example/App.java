7dd5b68c561cccf7526f43681ef323bc340caf16
mike-neck <shinya.mochida@l-is-b.com>
2019-04-15 12:52:27 +0900

    doma プロジェクト設定

---
+++ spring-db/src/main/java/com/example/App.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+
+  @Bean
+  CommandLineRunner commandLineRunner() {
+    return args -> {};
+  }
+}
