4d9469b432c62047724af7927fafdd5404e183b3
mike-neck <shinya.mochida@l-is-b.com>
2019-04-17 13:19:24 +0900

    アーキテクチャのテストを追加した

---
+++ spring-web/src/test/java/com/example/ModelArchitectureTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+
+@AnalyzeClasses(packages = "com.example.model")
+class ModelArchitectureTest {
+
+  @Test
+  void modelDidNotDependOnAnyPackages() {
+    final JavaClasses classes = new ClassFileImporter().importPackages("com.example.model");
+
+    final ArchRule rule = classes()
+        .should().accessClassesThat().resideInAnyPackage("java.lang", "java.util","java.time", "com.example.model")
+        .because("model should be independent of any components");
+
+    rule.check(classes);
+  }
+}
