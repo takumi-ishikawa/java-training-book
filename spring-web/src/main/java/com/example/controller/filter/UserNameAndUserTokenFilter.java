07dedb9dc61b0981a7a71d36448455cfae41b7c9
l-is-b_ishikawa <takumi.ishikawa@l-is-b.com>
2019-05-08 16:39:14 +0900

    Filter の実装を勘違いしていたので再度実装。 userName と userToken が合わない場合は例外投げるようにした。

---
+++ spring-web/src/main/java/com/example/controller/filter/UserNameAndUserTokenFilter.java
@@ -0,0 +1,41 @@
+package com.example.controller.filter;
+
+import com.example.model.UserName;
+import com.example.model.UserRepository;
+import com.example.model.UserToken;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class UserNameAndUserTokenFilter extends OncePerRequestFilter {
+
+  private Logger logger = LoggerFactory.getLogger(UserNameAndUserTokenFilter.class);
+  private final UserRepository userRepository;
+
+  @Contract(pure = true)
+  public UserNameAndUserTokenFilter(@NotNull final UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    final String userToken = request.getHeader("X-USER-TOKEN");
+    final String uri = request.getRequestURI();
+    final String[] splitedUri = uri.split("/");
+    final String userName = splitedUri[2];
+    if (!request.getMethod().equals("GET")) {
+      userRepository.findUserByUserNameAndUserToken(UserName.of(userName), UserToken.of(userToken));
+    }
+    filterChain.doFilter(request, response);
+  }
+}
