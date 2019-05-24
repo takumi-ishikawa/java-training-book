f86dd9b63415ca3dd403a06785387efe579c851b
l-is-b_ishikawa <takumi.ishikawa@l-is-b.com>
2019-05-24 11:18:05 +0900

    トークンのテストを追加

--- spring-web/src/test/java/com/example/dao/UserTokenDaoTest.java
+++ spring-web/src/test/java/com/example/dao/UserTokenDaoTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.example.model.UserId;
+import com.example.model.UserToken;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,7 +32,19 @@ class UserTokenDaoTest {
   void UserIdが1000の場合にUserIdが1000のUserTokenEntityが返ってくる() {
     assertThat(userTokenDao.findUserTokenByUserId(UserId.of(1000L)))
         .hasValueSatisfying(
-            userTokenEntity -> assertThat(userTokenEntity.userId).isEqualTo(UserId.of(1000L)));
+            userTokenEntity -> {
+              assertThat(userTokenEntity.userId).isEqualTo(UserId.of(1000L));
+            });
+  }
+
+  @Test
+  void UserIdが1000の場合にそれに対応するUserTokenをもつUserTokenEntityが返ってくる() {
+    assertThat(userTokenDao.findUserTokenByUserId(UserId.of(1000L)))
+        .hasValueSatisfying(
+            userTokenEntity -> {
+              assertThat(userTokenEntity.token)
+                  .isEqualTo(UserToken.of("838128c4-acb1-46ba-a3ee-4c995cb0f57c"));
+            });
   }
 
   @Test
